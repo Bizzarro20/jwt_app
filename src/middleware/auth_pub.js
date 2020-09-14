b701fc7c3e909939eac8be959270089920aa0553
navjot <navjotmalhi95@gmail.com>
2020-09-14 11:31:13 +0200

    fine fine ... mancano togliere commenti inutili

--- src/middleware/auth_pub.js
+++ src/middleware/auth_pub.js
@@ -3,23 +3,23 @@
 //serve per fare quelle cose che servono (controlli) prima che la richiesta arrivi
 //al server e che il server ritorni la risposta
 
-const jwt = require('jsonwebtoken')
-const Pub = require('../models/Pub')
+const jwt = require('jsonwebtoken');
+const Pub = require('../models/Pub');
 
 const auth = async(req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '')  //prendiamo dal header il nostro token jwt (jwt token sono tipo bearer)
-    const data = jwt.verify(token, process.env.JWT_KEY)    //verifca il token dalla sessione con la key segreta
+    const token = req.header('Authorization').replace('Bearer ', '');  //prendiamo dal header il nostro token jwt (jwt token sono tipo bearer)
+    const data = jwt.verify(token, process.env.JWT_KEY);    //verifca il token dalla sessione con la key segreta
     try {                                                   //fa una decodifica del token
-        const pub = await Pub.findOne({ _id: data._id, 'tokens.token': token })  //cerco nel db un pub che ha quel token
+        const pub = await Pub.findOne({ _id: data._id, 'tokens.token': token });  //cerco nel db un pub che ha quel token
         if (!pub) {
-            throw new Error()           //chi fa la richiesta non ha un token valido
+            throw new Error();           //chi fa la richiesta non ha un token valido
         }
-        req.pub = pub
-        req.token = token
+        req.pub = pub;
+        req.token = token;
         next()                      //next dice passa la richiesta al server.
     } catch (error) {
-        res.status(401).send({ error: 'Not authorized to access this resource' })
+        res.status(401).send({ error: 'Not authorized to access this resource' });
     }                                                                               //token non esiste o scaduto se ho messo scadenza
 
 }
-module.exports = auth
+module.exports = auth;
--- src/middleware/auth_user.js
+++ src/middleware/auth_user.js
@@ -4,23 +4,22 @@
 //al server e che il server ritorni la risposta
 
 
-const jwt = require('jsonwebtoken')
-const User = require('../models/User')
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const auth_user = async(req, res, next) => {
-    const token = req.header('Authorization').replace('Bearer ', '')  //prendiamo dal header il nostro token jwt (jwt token sono tipo bearer)
-    const data = jwt.verify(token, process.env.JWT_KEY)    //verifca il token dalla sessione con la key segreta
+    const token = req.header('Authorization').replace('Bearer ', '');  //prendiamo dal header il nostro token jwt (jwt token sono tipo bearer)
+    const data = jwt.verify(token, process.env.JWT_KEY);    //verifca il token dalla sessione con la key segreta
     try {                                                   //fa una decodifica del token
-        const user = await User.findOne({ _id: data._id, 'tokens.token': token })  //cerco nel db un user che ha quel token
+        const user = await User.findOne({ _id: data._id, 'tokens.token': token });  //cerco nel db un user che ha quel token
         if (!user) {
-            throw new Error()           //chi fa la richiesta non ha un token valido
+            throw new Error();           //chi fa la richiesta non ha un token valido
         }
-        req.user = user
-        req.token = token
-        next()                      //next dice passa la richiesta al server.
+        req.user = user;
+        req.token = token;
+        next();                      //next dice passa la richiesta al server.
     } catch (error) {
-        res.status(401).send({ error: 'Not authorized to access this resource' })
+        res.status(401).send({ error: 'Not authorized to access this resource' });
     }                                                                               //token non esiste o scaduto se ho messo scadenza
-
 }
-module.exports = auth_user           //cosi possiamo usarlo globalmente
+module.exports = auth_user;           //cosi possiamo usarlo globalmente
